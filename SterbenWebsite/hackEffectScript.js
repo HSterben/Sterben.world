b30216ca94b0bfb21cdffd64669d8014b9b11331
HSterben <hxdisterben@gmail.com>
2024-01-18 23:50:15 -0500

    Added flame effect, added Hack effect + About me page

---
+++ SterbenWebsite/hackEffectScript.js
@@ -0,0 +1,19 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelectorAll(".hack").forEach(element => {
+    element.onmouseover = event => {
+        let iterations = 0;
+        const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+                .map((letter, index) => {
+                    if (index < iterations) {
+                        return event.target.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+            if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+            iterations += 1 / 3;
+        }, 30);
+    }
+});
